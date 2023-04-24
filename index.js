33343abe30551479b45ba673f95b2d5f532f5cec
Youkwhan <Youkwhan@gmail.com>
2023-04-24 17:38:26 -0400

    js

---
+++ index.js
@@ -0,0 +1,44 @@
+// Get color button
+document.getElementById("get-color").addEventListener("click", () => {
+	render();
+});
+
+// get color schemes
+function getColorSchemeHTML(data) {
+	return data.colors
+		.map((color) => {
+			return `<div style="background-color:${color.hex.value}"></div>`;
+		})
+		.join("");
+}
+
+// get color hex
+function getColorHexHTML(data) {
+	return data.colors
+		.map((color) => {
+			return `<div>${color.hex.value}</div>`;
+		})
+		.join("");
+}
+
+function render() {
+	const colorPickerEl = document.getElementById("color-picker");
+	const schemePickerEl = document.getElementById("scheme-picker");
+
+	const colorSchemeEl = document.getElementById("color-scheme");
+	const colorHexEl = document.getElementById("color-hex");
+
+	fetch(
+		`https://www.thecolorapi.com/scheme?hex=${colorPickerEl.value.substring(
+			1
+		)}&mode=${schemePickerEl.value}&count=5`,
+		{ method: "GET" }
+	)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			colorSchemeEl.innerHTML = getColorSchemeHTML(data);
+			colorHexEl.innerHTML = getColorHexHTML(data);
+		});
+}
+render();
